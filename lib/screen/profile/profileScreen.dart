debf8634c14aad974882eb9f8ca2957d304c97f0
AroshaPunchihewa <aroshapunchihewa@gmail.com>
2023-02-13 01:18:24 +0530

    add screens

---
+++ lib/screen/profile/profileScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+//Naflan
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
