71fabe61e0260fe59d191102124b16788eae91a4
AroshaPunchihewa <aroshapunchihewa@gmail.com>
2023-02-13 01:31:22 +0530

    add scaffold to rs

--- lib/screen/notification/notificationScreen.dart
+++ lib/screen/notification/notificationScreen.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+//janith
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
 
@@ -11,6 +12,6 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
